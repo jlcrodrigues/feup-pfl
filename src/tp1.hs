b1b0c584e9f2943e363cd7c266208c1a175b2614
luis <joseluiscunharodrigues@gmail.com>
2022-10-08 19:01:28 +0100

    Added polynomial sorting and addition

--- src/tp1.hs
+++ src/tp1.hs
@@ -14,9 +14,19 @@ getVariable (_, v, _) = v
 getExponent :: Mon -> Int
 getExponent (_, _, e) = e
 
+-- Determine if a monomial should appear first in normal order
+compareMon :: Mon -> Mon -> Ordering
+compareMon (_, v1, e1) (_, v2, e2) 
+    | (v1 == v2) = if (e1 >= e2) then GT else LT
+    | otherwise = if (v1 >= v2) then GT else LT
+
+-- Sort a polynomial according to its variable and exponent
+sortPol :: Pol -> Pol
+sortPol p = sortBy (\x y -> compareMon x y) p
+
 -- Group monomials with same variable and degree
 likeTerms :: Pol -> [Pol]
-likeTerms p = groupBy (\(_, v1, e1) (_, v2, e2) -> v1 == v2 && e1 == e2) p
+likeTerms p = groupBy (\(_, v1, e1) (_, v2, e2) -> (v1 == v2 && e1 == e2)) (sortPol p)
 
 -- Normalize polynomials
 polNormalize :: Pol -> Pol
@@ -24,7 +34,7 @@ polNormalize p = [(sum [c | (c, _, _)<-x], getVariable (x!!0), getExponent (x!!0
 
 -- Add polynomials
 polAdd :: Pol -> Pol -> Pol
-polAdd a b = a
+polAdd a b = polNormalize (a ++ b)
 
 -- Multiply polynomials
 polMultiply :: Pol -> Pol -> Pol
